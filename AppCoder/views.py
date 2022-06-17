6e50e4b29f92f6bc702538da58fc661c99466ba8
by3xtr4 <leonardoandresgandini@gmail.com>
2022-06-17 10:06:04 -0300

    Proyecto inicial, comitiado, master respaldado 17 de Junio

---
+++ AppCoder/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
+from AppCoder.models import Curso
+
+# Create your views here.
+
+def curso(request):
+
+      curso =  Curso(nombre="Desarrollo web", camada="19881")
+      
+      curso.save()
+      
+      curso2 = Curso(nombre="Desarrollo Back", camada="222222")
+      
+      curso2.save()
+      
+      documentoDeTexto = f"El Curso 1 es: {curso.nombre}   Camada: {curso.camada}, el curso 2 es: {curso2.nombre} Camada: {curso2.camada} "
+
+
+      return HttpResponse(documentoDeTexto)
+
+
+def inicio(request):
+
+      return render(request, "AppCoder/inicio.html")
+
+def cursos(request):
+
+      return render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+
+      return render(request, "AppCoder/profesores.html")
+
+
+def estudiantes(request):
+
+      return render(request, "AppCoder/estudiantes.html")
+
+
+def entregables(request):
+
+      return render(request, "AppCoder/entregables.html")
