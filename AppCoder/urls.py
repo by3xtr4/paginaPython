1748e8557ab47970d4b2f7402fa742496e6a3e7c
by3xtr4 <leonardoandresgandini@gmail.com>
2022-06-17 11:20:19 -0300

    inicio reparado, funcionalidad casi completa[C

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -4,7 +4,8 @@ from AppCoder import views
 
 urlpatterns = [
    
-    path('', views.inicio, name="Inicio"), #esta era nuestra primer view
+    path('', views.inicio), #esta era nuestra primer view
+    path('inicio', views.inicio, name="Inicio"), #esta era nuestra primer view
     path('cursos', views.cursos, name="Cursos"), #vista mejorada, 
     path('profesores', views.profesores, name="Profesores"),
     path('estudiantes', views.estudiantes, name="Estudiantes"),
